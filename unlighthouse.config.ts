a65bf5402d904075932bc1961af17c7e72c50d2b
arif-rachim <a.arif.r@gmail.com>
2025-05-21 21:18:56 +0400

    Update dependencies in `bun.lock` file
    Removed unused dependencies `prebuild-install`, `sharp`, and `chownr`. Downgraded `node-addon-api` and `sharp` versions and updated several dependency versions including `tar-fs`, `tar-stream`, and `chownr` for compatibility improvements.

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "unlighthouse";
  * https://unlighthouse.dev/api/config
  */
 export default defineConfig({
-	site: "majestico.co",
+	site: "skyve.ae",
 	scanner: {
 		exclude: [/^\/cdn-cgi\//],
 	},
