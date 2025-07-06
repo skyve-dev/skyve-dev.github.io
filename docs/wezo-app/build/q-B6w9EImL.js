e615950d0bb011329311a82484867d87e9efbb17
skyve-dev <skyvedev@gmail.com>
2025-07-07 00:51:02 +0400

    Update with latest version

---
+++ docs/wezo-app/build/q-B6w9EImL.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./q-BUuyHtLx.js";import{c as t,q as r}from"./q-DZXsFSfu.js";const e=t(r(()=>o(()=>import("./q-Cw9jN_B0.js"),[]),"s_tntnak2DhJ8"));export{e as default};
--- docs/wezo-app/build/q-XRbSxGzd.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./q-BUuyHtLx.js";import{c as t,q as r}from"./q-DZXsFSfu.js";const e=t(r(()=>o(()=>import("./q-DwSI81b7.js"),[]),"s_tntnak2DhJ8"));export{e as default};
